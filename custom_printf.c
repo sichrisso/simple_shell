9adb74af3855028b085425ef30d466769a082ebf
sichrisso <helenasolomon87@gmail.com>
2022-05-16 20:01:45 +0300

    custom printf

---
+++ custom_printf.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ * _strlen - Counts the length of a string
+ * @s:char
+ * Return:int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			continue;
+		}
+return (i);
+}
+
+/**
+ * _strchr - Locate Charactere In String
+ * @s:String Search In
+ * @c:Char To Search For
+ * Return: Pointer To Char*
+ */
+char *_strchr(char *s, char c)
+{
+
+	do		{
+
+		if (*s == c)
+			{
+			break;
+			}
+		}	while (*s++);
+
+return (s);
+}
+/**
+ * _strncmp - Compare Amount (n) Of Characters Of Two Strings.
+ * @s1: A String.
+ * @s2: A String.
+ * @n: Amount Of Characters To Compare.
+ *
+ * Return: 1 If The Strings Don't Match Otherwise 0
+ */
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	if (s1 == NULL)
+		return (-1);
+	for (i = 0; i < n && s2[i]; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+/**
+ * _strdup - Duplicate A String
+ * @str:String
+ * Return: Duplicate String Failed Null
+ */
+char *_strdup(char *str)
+{
+	size_t len, i;
+	char *str2;
+
+	len = _strlen(str);
+	str2 = malloc(sizeof(char) * (len + 1));
+	if (!str2)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= len; i++)
+	{
+		str2[i] = str[i];
+	}
+
+	return (str2);
+}
